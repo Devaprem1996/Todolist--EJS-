91657af270cc51fa4ea4c5a120f4e6e46c5fa01a
Devaprem1996 <109859523+Devaprem1996@users.noreply.github.com>
2022-09-16 21:51:05 +0530

    Add gitignore, Procfile and update ports

--- app.js
+++ app.js
@@ -26,7 +26,6 @@ const itemsSchema = {
     },
 
 };
-
 const Item = mongoose.model('item', itemsSchema);
 
 const listsSchema = {
@@ -150,6 +149,12 @@ app.post('/delete', function (req, res) {
 
 
 
-app.listen(3000, function(){
-  console.log("Server started on port 3000.");
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+
+app.listen(port, function(){
+  console.log("Server has started successfully.");
 });
